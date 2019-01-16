215402c9ff0fa2095ea0278d6be682cd9050eac2
cfx <you@example.com>
2019-01-16 10:16:13 +0800

    框架

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("敏感词管理系统\n");
+    while(1){
+        printf("1--添加敏感字符\n");
+        printf("2--删除最后一个敏感字符\n");
+        printf("3--显示所有敏感字符\n");
+        printf("4--替换敏感字符\n");
+        printf("5--退出\n");
+
+
+        printf("请输入所需要的数字\n");
+    }
+
+    return 0;
+
+}
